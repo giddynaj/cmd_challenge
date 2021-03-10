615d50ab7dd1b409ee92deefea608b96b07eea59
tom <tassinaro@groupsdp.com>
2021-03-10 12:21:29 -0500

    Adds initial reports files

--- app/jobs/import_patients_job.rb
+++ app/jobs/import_patients_job.rb
@@ -3,7 +3,12 @@ class ImportPatientsJob < ApplicationJob
 
   def perform(*args)
     raise StandardError.new "File does not exist" unless File.exists? args[0].to_s
+    report = File.new(Rails.root.join('report.txt'), "w+")
+      report.write "This is a report"
+    report.close
   rescue StandardError => e
     STDERR.puts e.message
+  else
+    STDOUT.puts "Job Completed"
   end
 end
--- test/jobs/import_patients_job_test.rb
+++ test/jobs/import_patients_job_test.rb
@@ -4,4 +4,9 @@ class ImportPatientsJobTest < ActiveJob::TestCase
   test "should fail if no file provided" do 
     assert_not ImportPatientsJob.perform_now
   end
+
+  test "should write a report if file is provided" do
+    ImportPatientsJob.perform_now Rails.root.join('test','fixtures','files','good.csv')
+    assert File.exists? Rails.root.join('report.txt')
+  end
 end
